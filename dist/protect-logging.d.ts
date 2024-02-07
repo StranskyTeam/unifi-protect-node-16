268fb94f4ae264caa0e8cc4fb1f7fba831fc35ab
Michael Posch <posch@stransky.co.at>
2024-02-07 11:00:19 +0100

    added dist folder to .gitignore removed dist folder

--- dist/protect-logging.d.ts
+++
@@ -1,6 +0,0 @@
-export interface ProtectLogging {
-    debug(message: string, ...parameters: unknown[]): void;
-    error(message: string, ...parameters: unknown[]): void;
-    info(message: string, ...parameters: unknown[]): void;
-    warn(message: string, ...parameters: unknown[]): void;
-}
